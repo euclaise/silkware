787e8d8918b7e4438c7f8e5fb080770288017f8a
euclaise <jpritsk@purdue.edu>
2023-03-28 10:37:55 -0400

    Use acpispec headers

--- kernel/arch/x86_64/acpi.h
+++ kernel/arch/x86_64/acpi.h
@@ -1,31 +1,10 @@
 #include <stdint.h>
+#include <acpispec/tables.h>
 
-typedef struct rsdp_desc
-{
-    char signature[8];
-    uint8_t checksum;
-    char oem_id[6];
-    uint8_t revision;
-    uint32_t addr;
-} __attribute__ ((packed)) rsdp_desc;
-
-extern rsdp_desc rsdp;
-
-typedef struct sdt_header
-{
-    char signature[4];
-    uint32_t length;
-    uint8_t revision;
-    uint8_t checksum;
-    char oem_id[6];
-    char oem_table_id[8];
-    uint32_t oem_revision;
-    uint32_t creator_id;
-    uint32_t creator_revision;
-} sdt_header;
+extern acpi_rsdp_t rsdp;
 
 typedef struct rsdt_t
 {
-    struct sdt_header h;
+    acpi_header_t h;
     uint32_t p[];
-} rsdt_t;
+} __attribute__((packed)) rsdt_t;
