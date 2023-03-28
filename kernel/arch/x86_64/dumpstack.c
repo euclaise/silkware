a171c40e1ac171baab159198019669009b8d3c25
euclaise <jpritsk@purdue.edu>
2023-03-28 13:11:43 -0400

    stack trace code

--- kernel/arch/x86_64/dumpstack.c
+++ kernel/arch/x86_64/dumpstack.c
@@ -5,18 +5,18 @@ struct frame
 {
     struct frame *rbp;
     uint64_t rip;
-};
+} __attribute__((packed));
 
 void dump_stack(void)
 {
     struct frame *stk;
-    __asm__ volatile ("mov %%rsp,%0" : "=r" (stk));
+    __asm__ volatile ("mov %%rbp,%0" : "=r" (stk));
     printf("Stack trace:\n");
 
 
-    for (int i = 0; stk && i < 5; ++i)
+    for (int i = 0; stk >= (struct frame *) 0xffffffff80000000 && i < 5; ++i)
     {
-        printf("    0x%16lx\n", stk->rip);
+        printf("    %p\n", stk->rip);
         stk = stk->rbp;
     }
 }
