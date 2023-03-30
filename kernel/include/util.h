c79a56b909c1f9b85dd2232d7a50bdef3a9e1a43
euclaise <jpritsk@purdue.edu>
2023-03-29 20:39:15 -0400

    portability

--- kernel/include/util.h
+++ kernel/include/util.h
@@ -1,4 +1,14 @@
+#ifndef UTIL_H
+#define UTIL_H
+
 #ifdef __GNUC__
 #define likely(x)   __builtin_expect(!!(x),1)
 #define unlikely(x) __builtin_expect((x),0)
+#define UNREACHABLE __builtin_unreachable()
+#else
+#define likely(x)
+#define unlikely(x)
+#define UNREACHABLE while (1)
+#endif
+
 #endif
