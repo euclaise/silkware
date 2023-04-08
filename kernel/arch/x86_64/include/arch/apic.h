df225ff98a8b07c17c6471c89263d9cf582e1b5c
euclaise <jpritsk@purdue.edu>
2023-04-08 13:55:59 -0400

    tweaks

--- kernel/arch/x86_64/include/arch/apic.h
+++ kernel/arch/x86_64/include/arch/apic.h
@@ -5,12 +5,19 @@
 #include <util.h>
 #include <acpispec/tables.h>
 
+struct record
+{
+    int8_t type;
+    int8_t len;
+    int8_t data[1];
+} _packed;
+
 struct madt
 {
     acpi_header_t h;
     uint32_t lapic_addr;
     uint32_t flags;
-    int8_t data[1];
+    struct record data[1];
 } _packed;
 
 void apic_init(void);
