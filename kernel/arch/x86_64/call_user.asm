091962c1e05ec341a3df2b52febcf2a4ab71c138
euclaise <jpritsk@purdue.edu>
2023-04-12 09:32:09 -0400

    Restore RSP

--- kernel/arch/x86_64/call_user.asm
+++ kernel/arch/x86_64/call_user.asm
@@ -5,7 +5,10 @@ call_user:
     mov rcx, 0x100000
     mov r11, 0x202
     mov rdi, 100
+
     lea rax, [rel cpu_data + CPU_STACK_OFF]
     mov [rax], rsp
+
     mov rsp, 0xFFFFF000
+
     o64 sysret
